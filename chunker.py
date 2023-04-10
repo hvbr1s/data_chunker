9b5c439632b034d610b3aef1163267be0b9e11d3
Hvbris <44342309+hvbr1s@users.noreply.github.com>
2023-04-10 11:38:29 +0200

    Update chunker.py

--- chunker.py
+++ chunker.py
@@ -2,15 +2,20 @@ from langchain.document_loaders import UnstructuredFileLoader
 import tiktoken
 import matplotlib.pyplot as plt
 import seaborn as sns
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import hashlib
+from tqdm.auto import tqdm
+import json
 
 
 loader = UnstructuredFileLoader("/home/dan/langchain_projects/chunker/cal.html")
 docs = loader.load()
-
 print(len(docs))
 
-
 tokenizer = tiktoken.get_encoding('cl100k_base')
+m = hashlib.md5()
+
+documents = []
 
 # create the length function
 def tiktoken_len(text):
@@ -20,8 +25,8 @@ def tiktoken_len(text):
     )
     return len(tokens)
 
-token_counts = [tiktoken_len(doc.page_content) for doc in docs]
-print(token_counts)
+#token_counts = [tiktoken_len(doc.page_content) for doc in docs]
+#print(token_counts)
 
 # # set style and color palette for the plot
 # sns.set_style("whitegrid")
@@ -37,3 +42,39 @@ print(token_counts)
 # plt.ylabel("Frequency")
 
 # plt.show()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=400,
+    chunk_overlap=20,  # number of tokens overlap between chunks
+    length_function=tiktoken_len,
+    separators=['\n\n', '\n', ' ', '']
+)
+
+chunks = text_splitter.split_text(docs[0].page_content)
+print(len(chunks))
+
+for doc in tqdm(docs):
+    url = doc.metadata['source'].replace('rtdocs/', 'https://')
+    m.update(url.encode('utf-8'))
+    uid = m.hexdigest()[:12]
+    chunks = text_splitter.split_text(doc.page_content)
+    for i, chunk in enumerate(chunks):
+        documents.append({
+            'id': f'{uid}-{i}',
+            'text': chunk,
+            'source': url
+        })
+
+print(len(documents))
+
+with open('train.jsonl', 'w') as f:
+    for doc in documents:
+        f.write(json.dumps(doc) + '\n')
+
+with open('train.jsonl', 'r') as f:
+    for line in f:
+        documents.append(json.loads(line))
+
+len(documents)
+
+print(documents[10])
